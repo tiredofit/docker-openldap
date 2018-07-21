6fa7cb56f76c83cd6fbf17efca98cf6736599751
Dave Conroy <dave@tiredofit.ca>
2018-07-20 19:28:44 -0700

    Update Zabbix Templates

--- install/etc/zabbix/zabbix_agentd.conf.d/scripts/ldap-stats.sh
+++ install/etc/zabbix/zabbix_agentd.conf.d/scripts/ldap-stats.sh
@@ -1,17 +1,25 @@
-#!/bin/bash
+#!/usr/bin/with-contenv bash
+
+# if BASE_DN is empty set value from DOMAIN
+  if [ -z "$BASE_DN" ]; then
+    IFS='.' read -ra BASE_DN_TABLE <<< "$DOMAIN"
+    for i in "${BASE_DN_TABLE[@]}"; do
+      EXT="dc=$i,"
+      BASE_DN=$BASE_DN$EXT
+    done
+
+    BASE_DN=${BASE_DN::-1}
+  fi
 
 #config
-LDAP_SERVER="ldap://localhost"
-LDAP_PORT="389"
-LDAP_USER="cn=admin,<LDAP_BASE_DN>"
-LDAP_PASS="<LDAP_ADMIN_PASSWORD>"
+URI="ldap://127.0.0.1:389"
 
 #dynamic
 LDAP_PARAM="$1"
 LDAP_RESPONSE_KEY="${2:-monitorCounter}"
 
 #get LDAP response
-COMMAND="ldapsearch -H $LDAP_SERVER:$LDAP_PORT -b $LDAP_PARAM -D $LDAP_USER -w $LDAP_PASS"
+COMMAND="ldapsearch -H $URI -b $LDAP_PARAM -D cn=admin,$BASE_DN -w $CONFIG_PASS"
 RAW=$($COMMAND -s base '(objectClass=*)' '*' '+')
 
 #parse out number
