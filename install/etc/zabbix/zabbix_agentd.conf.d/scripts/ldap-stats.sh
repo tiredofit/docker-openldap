e18830cc457dcab4521329ffa50c3e34ba6ad727
Felix Haase <felix.haase@feki.de>
2020-01-09 12:16:43 +0100

    Add secrets support

--- install/etc/zabbix/zabbix_agentd.conf.d/scripts/ldap-stats.sh
+++ install/etc/zabbix/zabbix_agentd.conf.d/scripts/ldap-stats.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/with-contenv bash
 
+function file_env () {
+  local var="$1"
+  local fileVar="${var}_FILE"
+  local def="${2:-}"
+  local val="$def"
+  if [ "${!fileVar:-}" ]; then
+    val="$(cat "${!fileVar}")"
+  elif [ "${!var:-}" ]; then
+    val="${!var}"
+  fi
+  if [ -z ${val} ]; then
+    print_error "error: neither $var nor $fileVar are set but are required"
+    exit 1
+  fi
+  export "$var"="$val"
+  unset "$fileVar"
+}
+
 # if BASE_DN is empty set value from DOMAIN
   if [ -z "$BASE_DN" ]; then
     IFS='.' read -ra BASE_DN_TABLE <<< "$DOMAIN"
@@ -11,6 +29,7 @@
     BASE_DN=${BASE_DN::-1}
   fi
 
+file_env 'ADMIN_PASS'
 
 #dynamic
 LDAP_PARAM="$1"
